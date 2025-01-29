f0248d52db38e8c81e9f7cfce90f86d5d3c76fdb
Mounark <douidymohammed999@gmail.com>
2025-01-29 21:37:59 +0100

    Found that the data has to be initialized first hand before the execution of the program, so the vertices data is now declared as constexpr in Cube.h

--- src/main.cpp
+++ src/main.cpp
@@ -81,18 +81,14 @@ int main() {
   const Shader textureShader("../res/shaders/textured_cube_shader.vert",
                              "../res/shaders/textured_cube_shader.frag");
 
-  std::vector<TexturedCube> TexturedCubes;
-  for (unsigned int i = 0; i < 1; i++) {
-    TexturedCubes.emplace_back(
-        glm::vec3(0.0f + static_cast<float>(i), 0.0f, 0.0f),
-        "../res/textures/container2.png", GL_RGBA,
-        "../res/textures/container2_specular.png", GL_RGBA,
-        "../res/textures/matrix.jpg", GL_RGB);
-  }
-
   const ColoredCube lightCube(glm::vec3(4.0f, 2.0f, -5.7f),
-                            glm::vec3(1.0f, 1.0f, 1.0f));
+                                glm::vec3(1.0f, 1.0f, 1.0f));
 
+  const TexturedCube texturedCube(
+            glm::vec3(0.0f, 0.0f, 0.0f),
+            "../res/textures/container2.png", GL_RGBA,
+            "../res/textures/container2_specular.png", GL_RGBA,
+            "../res/textures/matrix.jpg", GL_RGB);
   // add a scope here to destroy the vertex buffers/array before terminating the
   // opengl context
   {
@@ -114,14 +110,11 @@ int main() {
       glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-      textureShader.setVec3("lightPos", 0.0f, 1.0f, -4.7f);
       textureShader.setVec3("viewPos", camera.mPosition.x, camera.mPosition.y,
                             camera.mPosition.z);
 
       lightCube.draw(lightShader, camera);
-      for (const auto& cube: TexturedCubes) {
-        cube.draw(textureShader, camera);
-      }
+      texturedCube.draw(textureShader, camera);
 
       // glfw: swap buffers and poll IO events (keys pressed/released, mouse
       // moved etc.)
