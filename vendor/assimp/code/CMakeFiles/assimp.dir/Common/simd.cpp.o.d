95d50b77b5541c28ec147f6e12bd3cc162086f9a
Mounark <douidymohammed999@gmail.com>
2025-02-05 16:18:57 +0100

    Built Assimp files, next we'll work on meshes and models

---
+++ vendor/assimp/code/CMakeFiles/assimp.dir/Common/simd.cpp.o.d
@@ -0,0 +1,6 @@
+code/CMakeFiles/assimp.dir/Common/simd.cpp.o: \
+ /home/moha/Bureau/projets/projets_perso/opengl-renderer/vendor/assimp/code/Common/simd.cpp \
+ /usr/include/stdc-predef.h \
+ /home/moha/Bureau/projets/projets_perso/opengl-renderer/vendor/assimp/code/Common/simd.h \
+ /home/moha/Bureau/projets/projets_perso/opengl-renderer/vendor/assimp/include/assimp/defs.h \
+ /home/moha/Bureau/projets/projets_perso/opengl-renderer/vendor/assimp/include/assimp/config.h
